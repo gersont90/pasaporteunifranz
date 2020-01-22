418083a03072f62df590e5fbb114dd07e27b4ea4
gerson torrez <gersontorrez680@gmail.com>
2020-01-21 20:05:10 -0400

    cambio3

---
+++ src/app/admin/tabs/tabs-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children:[
+      {
+        path:'perfil',
+        loadChildren:'../tab1/tab1.module#Tab1PageModule'
+      },
+      {
+        path:'lector',
+        loadChildren:'../tab2/tab2.module#Tab2PageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  
+  {
+    path: 'tabs',
+    loadChildren: () => import('./admin/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./admin/tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./admin/tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./admin/tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
 ];
 
 @NgModule({
